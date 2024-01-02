b7f4e646b11a58b55b1005a5f8d0b5cc52b25239
son tran <quangson193@gmail.com>
2024-01-03 01:12:15 +0700

    update

---
+++ CSDLGia_ASP/Migrations/20240102175431_themtruongtest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CSDLGia_ASP.Migrations
+{
+    public partial class themtruongtest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "test",
+                table: "GiaDatPhanLoaiCt",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "test",
+                table: "GiaDatPhanLoaiCt");
+        }
+    }
+}
---
+++ CSDLGia_ASP/Migrations/20240102175720_themloadai.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CSDLGia_ASP.Migrations
+{
+    public partial class themloadai : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "test",
+                table: "GiaDatPhanLoaiCt",
+                newName: "Loaidat");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Loaidat",
+                table: "GiaDatPhanLoaiCt",
+                newName: "test");
+        }
+    }
+}
