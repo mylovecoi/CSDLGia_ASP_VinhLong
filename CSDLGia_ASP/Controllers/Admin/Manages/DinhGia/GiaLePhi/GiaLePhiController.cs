ed26d2af0fba86ae68cbd7ca4b82acd00c24e26b
Nguyễn Đăng Hùng Anh <49970242+iluvmanu95@users.noreply.github.com>
2024-03-31 16:37:39 +0700

    update-gialephitb-dsdonvi

--- CSDLGia_ASP/Controllers/Admin/Manages/DinhGia/GiaLePhi/GiaLePhiController.cs
+++ CSDLGia_ASP/Controllers/Admin/Manages/DinhGia/GiaLePhi/GiaLePhiController.cs
@@ -27,42 +27,68 @@ namespace CSDLGia_ASP.Controllers.Admin.Manages.DinhGia.GiaLePhi
 
         [Route("DinhGiaLePhi")]
         [HttpGet]
-        public IActionResult Index(string Madv, int Nam)
+        public IActionResult Index(string Madv, string Nam)
         {
             if (!string.IsNullOrEmpty(HttpContext.Session.GetString("SsAdmin")))
             {
                 if (Helpers.CheckPermission(HttpContext.Session, "csdlmucgiahhdv.lephi.thongtin", "Index"))
                 {
 
-                    var dsdonvi = (from db in _db.DsDiaBan.Where(t => t.Level != "H")
+                    var dsdonvi = (from db in _db.DsDiaBan.Where(t => t.Level != "ADMIN")
                                    join dv in _db.DsDonVi.Where(t => t.ChucNang != "QUANTRI") on db.MaDiaBan equals dv.MaDiaBan
                                    select new VMDsDonVi
                                    {
                                        Id = dv.Id,
                                        TenDiaBan = db.TenDiaBan,
-                                       MaDv = dv.MaDv
+                                       TenDv = dv.TenDv,
+                                       MaDv = dv.MaDv,
                                    }).ToList();
                     if (dsdonvi.Count > 0)
                     {
+                        Madv = string.IsNullOrEmpty(Madv) ? "all" : Madv;
                         if (Helpers.GetSsAdmin(HttpContext.Session, "Madv") != null)
                         {
                             Madv = Helpers.GetSsAdmin(HttpContext.Session, "Madv");
+                        }
 
+                        IEnumerable<CSDLGia_ASP.Models.Manages.DinhGia.GiaPhiLePhi> model = _db.GiaPhiLePhi;
+
+                        if (Madv != "all")
+                        {
+                            model = model.Where(t => t.Madv == Madv);
+                        }
+
+                        if (string.IsNullOrEmpty(Nam))
+                        {
+                            Nam = Helpers.ConvertYearToStr(DateTime.Now.Year);
+                            model = model.Where(t => t.Thoidiem.Year == int.Parse(Nam));
                         }
                         else
                         {
-                            if (string.IsNullOrEmpty(Madv))
+                            if (Nam != "all")
                             {
-                                Madv = dsdonvi.OrderBy(t => t.Id).Select(t => t.MaDv).First();
+                                model = model.Where(t => t.Thoidiem.Year == int.Parse(Nam));
                             }
                         }
-                        var model = _db.GiaPhiLePhi.Where(t => t.Madv == Madv);
 
-                        if (Nam != 0)
+                        if (Helpers.GetSsAdmin(HttpContext.Session, "Madv") == null)
                         {
-                            model = model.Where(t=>t.Thoidiem.Year == Nam);
+                            ViewData["DsDonVi"] = dsdonvi;
                         }
-                       
+                        else
+                        {
+                            ViewData["DsDonVi"] = dsdonvi.Where(t => t.MaDv == Madv);
+                        }
+                        var dsDonViTH = (from donvi in _db.DsDonVi
+                                         join tk in _db.Users on donvi.MaDv equals tk.Madv
+                                         join gr in _db.GroupPermissions.Where(x => x.ChucNang == "TONGHOP") on tk.Chucnang equals gr.KeyLink
+                                         select new CSDLGia_ASP.Models.Systems.DsDonVi
+                                         {
+                                             MaDiaBan = donvi.MaDiaBan,
+                                             MaDv = donvi.MaDv,
+                                             TenDv = donvi.TenDv,
+                                         });
+                        ViewData["DsDonViTh"] = dsDonViTH;
                         ViewData["NhomDm"] = _db.GiaPhiLePhiNhom;
                         ViewData["Madv"] = Madv;
                         ViewData["Nam"] = Nam;
