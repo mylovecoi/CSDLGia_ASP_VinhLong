60bd4e15d5cf24947ed45fbddaee78b63c8492ee
lifephongphanmem <61262850+lifephongphanmem@users.noreply.github.com>
2024-04-01 18:45:22 +0700

    fix Thẩm định giá

---
+++ CSDLGia_ASP/Migrations/20240401113334_thamdinhgiact-20240104.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CSDLGia_ASP.Migrations
+{
+    public partial class thamdinhgiact20240104 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Sl",
+                table: "ThamDinhGiaCt",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Sl",
+                table: "ThamDinhGiaCt",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
