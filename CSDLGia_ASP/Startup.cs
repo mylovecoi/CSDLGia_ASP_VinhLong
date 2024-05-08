42da1377ba10a8d751b56e23418463717fa622a0
Nguyễn Đăng Hùng Anh <49970242+iluvmanu95@users.noreply.github.com>
2024-05-08 14:43:17 +0700

    update-dulieutaphuan

--- CSDLGia_ASP/Startup.cs
+++ CSDLGia_ASP/Startup.cs
@@ -51,7 +51,7 @@ namespace CSDLGia_ASP
                 options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
             });
-            
+
             services.AddScoped<IDsDiaBanService, DsDiaBanService>();
             services.AddScoped<IDsDonviService, DsDonviService>();
             services.AddScoped<ITrangThaiHoSoService, TrangThaiHoSoService>();
@@ -61,55 +61,6 @@ namespace CSDLGia_ASP
             {
                 options.Conventions.AddPageRoute("/Login", "");
             });
-
-            //services.Configure<RequestLocalizationOptions>(options =>
-            //{
-            //    options.DefaultRequestCulture = new RequestCulture("vi-VN");
-            //});
-            //services.AddMvc();
-
-            /*services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = Configuration["JWT:ValidIssuer"],
-                    ValidAudience = Configuration["JWT:ValidAudience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:SecretKey"]))
-                };
-            });
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiCsdlGia2", Version = "v1" });
-
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-                {
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                });
-
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference=new OpenApiReference
-                            {
-                                Type=ReferenceType.SecurityScheme,
-                                Id="Bearer"
-                            }
-                        },
-                        new string[]{}
-                    }
-                });
-            });*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -118,8 +69,6 @@ namespace CSDLGia_ASP
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                /*app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiCsdlGia2 v1"));*/
             }
             else
             {
@@ -130,10 +79,6 @@ namespace CSDLGia_ASP
             app.UseSession();
             app.UseRouting();
             app.UseAuthorization();
-            //app.UseMvc(routes =>
-            //{
-            //    routes.MapRoute("default", "{HeThong}/{controller=CongBo}/{action=Index}");
-            //});
 
 
             app.UseEndpoints(endpoints =>
@@ -141,11 +86,6 @@ namespace CSDLGia_ASP
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}");
-                //endpoints.MapDefaultControllerRoute();
-                //endpoints.MapGet("/", context =>
-                //{
-                //    return Task.Run(() => context.Response.Redirect("/CongBo"));
-                //});
             });
         }
     }
