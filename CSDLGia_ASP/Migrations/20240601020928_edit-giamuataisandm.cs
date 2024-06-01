35babcb2dabbbc99bd60089229a1cc41a1b2661f
QuangSon <quangson.laptrinh@gmail.com>
2024-06-01 09:44:45 +0700

    update

---
+++ CSDLGia_ASP/Migrations/20240601020928_edit-giamuataisandm.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSDLGia_ASP.Migrations
+{
+    /// <inheritdoc />
+    public partial class editgiamuataisandm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Manhom",
+                table: "GiaMuaTaiSanDm");
+
+            migrationBuilder.DropColumn(
+                name: "Phanloai",
+                table: "GiaMuaTaiSanDm");
+
+            migrationBuilder.DropColumn(
+                name: "Stt",
+                table: "GiaMuaTaiSanDm");
+
+            migrationBuilder.RenameColumn(
+                name: "Tennhom",
+                table: "GiaMuaTaiSanDm",
+                newName: "Mota");
+
+            migrationBuilder.AddColumn<double>(
+                name: "KhoiLuong",
+                table: "GiaMuaTaiSanDm",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KhoiLuong",
+                table: "GiaMuaTaiSanDm");
+
+            migrationBuilder.RenameColumn(
+                name: "Mota",
+                table: "GiaMuaTaiSanDm",
+                newName: "Tennhom");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Manhom",
+                table: "GiaMuaTaiSanDm",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phanloai",
+                table: "GiaMuaTaiSanDm",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Stt",
+                table: "GiaMuaTaiSanDm",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ CSDLGia_ASP/Migrations/20240601021212_edit-giamuataisanndm2.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSDLGia_ASP.Migrations
+{
+    /// <inheritdoc />
+    public partial class editgiamuataisanndm2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HienThi",
+                table: "GiaMuaTaiSanDm",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "SapXep",
+                table: "GiaMuaTaiSanDm",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Style",
+                table: "GiaMuaTaiSanDm",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HienThi",
+                table: "GiaMuaTaiSanDm");
+
+            migrationBuilder.DropColumn(
+                name: "SapXep",
+                table: "GiaMuaTaiSanDm");
+
+            migrationBuilder.DropColumn(
+                name: "Style",
+                table: "GiaMuaTaiSanDm");
+        }
+    }
+}
