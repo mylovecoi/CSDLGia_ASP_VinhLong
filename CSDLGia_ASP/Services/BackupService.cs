9e05b20efce4026024db51046146a52c035139e8
Nguyễn Đăng Hùng Anh <49970242+iluvmanu95@users.noreply.github.com>
2024-06-18 16:40:53 +0700

    update-backup-restore

---
+++ CSDLGia_ASP/Services/BackupService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System;
+
+namespace CSDLGia_ASP.Services 
+{
+
+    public class BackupService
+    {
+        private readonly string _connectionString;
+
+        public BackupService(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("CSDLGia_ASPConnection");
+        }
+
+        public void BackupDatabase(string backupPath)
+        {
+            if (!System.IO.Directory.Exists(System.IO.Path.GetDirectoryName(backupPath)))
+            {
+                throw new Exception("Backup directory does not exist.");
+            }
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var query = $"BACKUP DATABASE CSDLGIAKH_ASP_DEMO TO DISK = '{backupPath}'";
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+
+
+}
+
+
+
+
+
