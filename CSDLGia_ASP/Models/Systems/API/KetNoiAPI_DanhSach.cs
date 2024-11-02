41248105d4c2e9019893800d7221a18c54d03b42
lifephongphanmem <61262850+lifephongphanmem@users.noreply.github.com>
2024-11-02 10:20:09 +0700

    update KetNOi-DanhSach

--- CSDLGia_ASP/Models/Systems/API/KetNoiAPI_DanhSach.cs
+++ CSDLGia_ASP/Models/Systems/API/KetNoiAPI_DanhSach.cs
@@ -18,11 +18,11 @@ namespace CSDLGia_ASP.Models.Systems.API
         public string LinkNhanPut { get; set; }
         public string Ghichu { get; set; }
         //Trường dữ liệu
-        //public string MaBM { get; set; }
-        //public string MaDiaBan { get; set; }
-        //public string MaDonVi { get; set; }
-        //public string NguoiTao { get; set; }
-        //public string NguoiDuyet { get; set; }
+        public string MaBM { get; set; }
+        public string MaDiaBan { get; set; }
+        public string MaDonVi { get; set; }
+        public string NguoiTao { get; set; }
+        public string NguoiDuyet { get; set; }
         public DateTime Created_at { get; set; }
         public DateTime Updated_at { get; set; }
 
