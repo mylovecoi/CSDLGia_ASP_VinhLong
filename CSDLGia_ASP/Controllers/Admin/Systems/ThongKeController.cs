aa98bc0832b9baf21c5718796907238965fa7985
son <quangson.laptrinh@gmail.com>
2024-11-06 15:15:52 +0700

    update

--- CSDLGia_ASP/Controllers/Admin/Systems/ThongKeController.cs
+++ CSDLGia_ASP/Controllers/Admin/Systems/ThongKeController.cs
@@ -30,20 +30,25 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
         public IActionResult Index(int Nam, string Madv, int Thang)
         {      
             // Giá thuê mặt đất mặt nước
+
+            // Nhập mới trong tháng
             var countGiaThueMatDatMatNuocCHTthang = _db.GiaThueMatDatMatNuoc.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             if (Nam > 0) { countGiaThueMatDatMatNuocCHTthang = countGiaThueMatDatMatNuocCHTthang.Where(x => x.Thoidiem.Year == Nam); }
             if (Thang > 0) { countGiaThueMatDatMatNuocCHTthang = countGiaThueMatDatMatNuocCHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaThueMatDatMatNuocCHTthang = countGiaThueMatDatMatNuocCHTthang.Count();
 
-            var countGiaThueMatDatMatNuocHTthang = _db.GiaThueMatDatMatNuoc.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            // Hoàn thành trong tháng
+            var countGiaThueMatDatMatNuocHTthang = _db.GiaThueMatDatMatNuoc. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             if (Nam > 0) { countGiaThueMatDatMatNuocHTthang = countGiaThueMatDatMatNuocHTthang.Where(x => x.Thoidiem.Year == Nam); }
             if (Thang > 0) { countGiaThueMatDatMatNuocHTthang = countGiaThueMatDatMatNuocHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaThueMatDatMatNuocHTthang = countGiaThueMatDatMatNuocHTthang.Count();
 
+            // Nhập mới tất cả
             var countGiaThueMatDatMatNuocCHT = _db.GiaThueMatDatMatNuoc.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaThueMatDatMatNuocCHT = countGiaThueMatDatMatNuocCHT.Count();
 
-            var countGiaThueMatDatMatNuocHT = _db.GiaThueMatDatMatNuoc.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv)); 
+            // Hoàn than tất cả
+            var countGiaThueMatDatMatNuocHT = _db.GiaThueMatDatMatNuoc. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv)); 
             int tongGiaThueMatDatMatNuocHT = countGiaThueMatDatMatNuocHT.Count();
 
 
@@ -53,7 +58,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             if (Thang > 0) { countGiaRungCHTthang = countGiaRungCHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaRungCHTthang = countGiaRungCHTthang.Count();
 
-            var countGiaRungHTthang = _db.GiaRung.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaRungHTthang = _db.GiaRung. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             if (Nam > 0) { countGiaRungHTthang = countGiaRungHTthang.Where(x => x.Thoidiem.Year == Nam); }
             if (Thang > 0) { countGiaRungHTthang = countGiaRungHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaRungHTthang = countGiaRungHTthang.Count();
@@ -61,7 +66,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             var countGiaRungCHT = _db.GiaRung.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaRungCHT = countGiaRungCHT.Count();
 
-            var countGiaRungHT = _db.GiaRung.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaRungHT = _db.GiaRung. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaRungHT = countGiaRungHT.Count();
 
 
@@ -71,7 +76,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             if (Thang > 0) { countGiaThueMuaNhaXhCHTthang = countGiaThueMuaNhaXhCHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaThueMuaNhaXhCHTthang = countGiaThueMuaNhaXhCHTthang.Count();
 
-            var countGiaThueMuaNhaXhHTthang = _db.GiaThueMuaNhaXh.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaThueMuaNhaXhHTthang = _db.GiaThueMuaNhaXh. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             if (Nam > 0) { countGiaThueMuaNhaXhHTthang = countGiaThueMuaNhaXhHTthang.Where(x => x.Thoidiem.Year == Nam); }
             if (Thang > 0) { countGiaThueMuaNhaXhHTthang = countGiaThueMuaNhaXhHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaThueMuaNhaXhHTthang = countGiaThueMuaNhaXhHTthang.Count();
@@ -79,7 +84,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             var countGiaThueMuaNhaXhCHT = _db.GiaThueMuaNhaXh.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaThueMuaNhaXhCHT = countGiaThueMuaNhaXhCHT.Count();
 
-            var countGiaThueMuaNhaXhHT = _db.GiaThueMuaNhaXh.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaThueMuaNhaXhHT = _db.GiaThueMuaNhaXh. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaThueMuaNhaXhHT = countGiaThueMuaNhaXhHT.Count();
 
 
@@ -89,7 +94,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             if (Thang > 0) { countGiaTaiSanCongCHTthang = countGiaTaiSanCongCHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaTaiSanCongCHTthang = countGiaTaiSanCongCHTthang.Count();
 
-            var countGiaTaiSanCongHTthang = _db.GiaTaiSanCong.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaTaiSanCongHTthang = _db.GiaTaiSanCong. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             if (Nam > 0) { countGiaTaiSanCongHTthang = countGiaTaiSanCongHTthang.Where(x => x.Thoidiem.Year == Nam); }
             if (Thang > 0) { countGiaTaiSanCongHTthang = countGiaTaiSanCongHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaTaiSanCongHTthang = countGiaTaiSanCongHTthang.Count();
@@ -97,7 +102,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             var countGiaTaiSanCongCHT = _db.GiaTaiSanCong.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaTaiSanCongCHT = countGiaTaiSanCongCHT.Count();
 
-            var countGiaTaiSanCongHT = _db.GiaTaiSanCong.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaTaiSanCongHT = _db.GiaTaiSanCong. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaTaiSanCongHT = countGiaTaiSanCongHT.Count();
 
 
@@ -107,7 +112,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             if (Thang > 0) { countGiaGiaoDichBDSCHTthang = countGiaGiaoDichBDSCHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaGiaoDichBDSCHTthang = countGiaGiaoDichBDSCHTthang.Count();
 
-            var countGiaGiaoDichBDSHTthang = _db.GiaGiaoDichBDS.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaGiaoDichBDSHTthang = _db.GiaGiaoDichBDS. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             if (Nam > 0) { countGiaGiaoDichBDSHTthang = countGiaGiaoDichBDSHTthang.Where(x => x.Thoidiem.Year == Nam); }
             if (Thang > 0) { countGiaGiaoDichBDSHTthang = countGiaGiaoDichBDSHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaGiaoDichBDSHTthang = countGiaGiaoDichBDSHTthang.Count();
@@ -115,7 +120,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             var countGiaGiaoDichBDSCHT = _db.GiaGiaoDichBDS.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaGiaoDichBDSCHT = countGiaGiaoDichBDSCHT.Count();
 
-            var countGiaGiaoDichBDSHT = _db.GiaGiaoDichBDS.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaGiaoDichBDSHT = _db.GiaGiaoDichBDS. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaGiaoDichBDSHT = countGiaGiaoDichBDSHT.Count();
 
             // Giá nước sinh hoạt
@@ -124,7 +129,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             if (Thang > 0) { countGiaNuocShCHTthang = countGiaNuocShCHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaNuocShCHTthang = countGiaNuocShCHTthang.Count();
 
-            var countGiaNuocShHTthang = _db.GiaNuocSh.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaNuocShHTthang = _db.GiaNuocSh. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             if (Nam > 0) { countGiaNuocShHTthang = countGiaNuocShHTthang.Where(x => x.Thoidiem.Year == Nam); }
             if (Thang > 0) { countGiaNuocShHTthang = countGiaNuocShHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaNuocShHTthang = countGiaNuocShHTthang.Count();
@@ -132,7 +137,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             var countGiaNuocShCHT = _db.GiaNuocSh.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaNuocShCHT = countGiaNuocShCHT.Count();
 
-            var countGiaNuocShHT = _db.GiaNuocSh.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaNuocShHT = _db.GiaNuocSh. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaNuocShHT = countGiaNuocShHT.Count();
 
 
@@ -142,7 +147,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             if (Thang > 0) { countGiaThueTaiSanCongCHTthang = countGiaThueTaiSanCongCHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaThueTaiSanCongCHTthang = countGiaThueTaiSanCongCHTthang.Count();
 
-            var countGiaThueTaiSanCongHTthang = _db.GiaThueTaiSanCong.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaThueTaiSanCongHTthang = _db.GiaThueTaiSanCong. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             if (Nam > 0) { countGiaThueTaiSanCongHTthang = countGiaThueTaiSanCongHTthang.Where(x => x.Thoidiem.Year == Nam); }
             if (Thang > 0) { countGiaThueTaiSanCongHTthang = countGiaThueTaiSanCongHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaThueTaiSanCongHTthang = countGiaThueTaiSanCongHTthang.Count();
@@ -150,7 +155,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             var countGiaThueTaiSanCongCHT = _db.GiaThueTaiSanCong.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaThueTaiSanCongCHT = countGiaThueTaiSanCongCHT.Count();
 
-            var countGiaThueTaiSanCongHT = _db.GiaThueTaiSanCong.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaThueTaiSanCongHT = _db.GiaThueTaiSanCong. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaThueTaiSanCongHT = countGiaThueTaiSanCongHT.Count();
 
             //Giá SP, DVCI, DVSNC, HH-DV đặt hàng
@@ -160,7 +165,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             if (Thang > 0) { countGiaSpDvCongIchCHTthang = countGiaSpDvCongIchCHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaSpDvCongIchCHTthang = countGiaSpDvCongIchCHTthang.Count();
 
-            var countGiaSpDvCongIchHTthang = _db.GiaSpDvCongIch.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaSpDvCongIchHTthang = _db.GiaSpDvCongIch. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             if (Nam > 0) { countGiaSpDvCongIchHTthang = countGiaSpDvCongIchHTthang.Where(x => x.Thoidiem.Year == Nam); }
             if (Thang > 0) { countGiaSpDvCongIchHTthang = countGiaSpDvCongIchHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaSpDvCongIchHTthang = countGiaSpDvCongIchHTthang.Count();
@@ -168,7 +173,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             var countGiaSpDvCongIchCHT = _db.GiaSpDvCongIch.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaSpDvCongIchCHT = countGiaSpDvCongIchCHT.Count();
 
-            var countGiaSpDvCongIchHT = _db.GiaSpDvCongIch.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaSpDvCongIchHT = _db.GiaSpDvCongIch. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaSpDvCongIchHT = countGiaSpDvCongIchHT.Count();
 
 
@@ -179,7 +184,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             if (Thang > 0) { countGiaDvGdDtCHTthang = countGiaDvGdDtCHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaDvGdDtCHTthang = countGiaDvGdDtCHTthang.Count();
 
-            var countGiaDvGdDtHTthang = _db.GiaDvGdDt.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaDvGdDtHTthang = _db.GiaDvGdDt. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             if (Nam > 0) { countGiaDvGdDtHTthang = countGiaDvGdDtHTthang.Where(x => x.Thoidiem.Year == Nam); }
             if (Thang > 0) { countGiaDvGdDtHTthang = countGiaDvGdDtHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaDvGdDtHTthang = countGiaDvGdDtHTthang.Count();
@@ -187,7 +192,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             var countGiaDvGdDtCHT = _db.GiaDvGdDt.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaDvGdDtCHT = countGiaDvGdDtCHT.Count();
 
-            var countGiaDvGdDtHT = _db.GiaDvGdDt.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaDvGdDtHT = _db.GiaDvGdDt. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaDvGdDtHT = countGiaDvGdDtHT.Count();
 
 
@@ -198,7 +203,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             if (Thang > 0) { countGiaDvKcbCHTthang = countGiaDvKcbCHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaDvKcbCHTthang = countGiaDvKcbCHTthang.Count();
 
-            var countGiaDvKcbHTthang = _db.GiaDvKcb.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaDvKcbHTthang = _db.GiaDvKcb. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             if (Nam > 0) { countGiaDvKcbHTthang = countGiaDvKcbHTthang.Where(x => x.Thoidiem.Year == Nam); }
             if (Thang > 0) { countGiaDvKcbHTthang = countGiaDvKcbHTthang.Where(x => x.Thoidiem.Month == Thang); }
             int tongGiaDvKcbHTthang = countGiaDvKcbHTthang.Count();
@@ -206,7 +211,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             var countGiaDvKcbCHT = _db.GiaDvKcb.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaDvKcbCHT = countGiaDvKcbCHT.Count();
 
-            var countGiaDvKcbHT = _db.GiaDvKcb.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaDvKcbHT = _db.GiaDvKcb. Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
             int tongGiaDvKcbHT = countGiaDvKcbHT.Count();
 
 
@@ -264,11 +269,6 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
                 countGiaDvKcbHTthang = tongGiaDvKcbHTthang,
                 countGiaDvKcbCHT = tongGiaDvKcbCHT,
                 countGiaDvKcbHT = tongGiaDvKcbHT,
-
-
-
-
-
             };
 
             ViewData["Title"] = " Thống kê hồ sơ của đơn vị hành chính";
