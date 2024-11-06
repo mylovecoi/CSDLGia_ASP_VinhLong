7500000b6afa0be18d868207da6ecd46290fe198
son <quangson.laptrinh@gmail.com>
2024-11-06 16:41:41 +0700

    update

--- CSDLGia_ASP/Controllers/Admin/Systems/ThongKeController.cs
+++ CSDLGia_ASP/Controllers/Admin/Systems/ThongKeController.cs
@@ -215,6 +215,194 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             int tongGiaDvKcbHT = countGiaDvKcbHT.Count();
 
 
+            //Mức trợ giá trợ cước
+
+            var countGiaTroGiaTroCuocCHTthang = _db.GiaTroGiaTroCuoc.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaTroGiaTroCuocCHTthang = countGiaTroGiaTroCuocCHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaTroGiaTroCuocCHTthang = countGiaTroGiaTroCuocCHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaTroGiaTroCuocCHTthang = countGiaTroGiaTroCuocCHTthang.Count();
+
+            var countGiaTroGiaTroCuocHTthang = _db.GiaTroGiaTroCuoc.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaTroGiaTroCuocHTthang = countGiaTroGiaTroCuocHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaTroGiaTroCuocHTthang = countGiaTroGiaTroCuocHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaTroGiaTroCuocHTthang = countGiaTroGiaTroCuocHTthang.Count();
+
+            var countGiaTroGiaTroCuocCHT = _db.GiaTroGiaTroCuoc.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaTroGiaTroCuocCHT = countGiaTroGiaTroCuocCHT.Count();
+
+            var countGiaTroGiaTroCuocHT = _db.GiaTroGiaTroCuoc.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaTroGiaTroCuocHT = countGiaTroGiaTroCuocHT.Count();
+
+
+            //Giá thuế tài nguyên
+
+            var countGiaThueTaiNguyenCHTthang = _db.GiaThueTaiNguyen.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaThueTaiNguyenCHTthang = countGiaThueTaiNguyenCHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaThueTaiNguyenCHTthang = countGiaThueTaiNguyenCHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaThueTaiNguyenCHTthang = countGiaThueTaiNguyenCHTthang.Count();
+
+            var countGiaThueTaiNguyenHTthang = _db.GiaThueTaiNguyen.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaThueTaiNguyenHTthang = countGiaThueTaiNguyenHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaThueTaiNguyenHTthang = countGiaThueTaiNguyenHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaThueTaiNguyenHTthang = countGiaThueTaiNguyenHTthang.Count();
+
+            var countGiaThueTaiNguyenCHT = _db.GiaThueTaiNguyen.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaThueTaiNguyenCHT = countGiaThueTaiNguyenCHT.Count();
+
+            var countGiaThueTaiNguyenHT = _db.GiaThueTaiNguyen.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaThueTaiNguyenHT = countGiaThueTaiNguyenHT.Count();
+
+
+            //Giá hàng hóa tại siêu thị
+
+            var countGiaHangHoaTaiSieuThiCHTthang = _db.GiaHangHoaTaiSieuThi.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaHangHoaTaiSieuThiCHTthang = countGiaHangHoaTaiSieuThiCHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaHangHoaTaiSieuThiCHTthang = countGiaHangHoaTaiSieuThiCHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaHangHoaTaiSieuThiCHTthang = countGiaHangHoaTaiSieuThiCHTthang.Count();
+
+            var countGiaHangHoaTaiSieuThiHTthang = _db.GiaHangHoaTaiSieuThi.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaHangHoaTaiSieuThiHTthang = countGiaHangHoaTaiSieuThiHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaHangHoaTaiSieuThiHTthang = countGiaHangHoaTaiSieuThiHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaHangHoaTaiSieuThiHTthang = countGiaHangHoaTaiSieuThiHTthang.Count();
+
+            var countGiaHangHoaTaiSieuThiCHT = _db.GiaHangHoaTaiSieuThi.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaHangHoaTaiSieuThiCHT = countGiaHangHoaTaiSieuThiCHT.Count();
+
+            var countGiaHangHoaTaiSieuThiHT = _db.GiaHangHoaTaiSieuThi.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaHangHoaTaiSieuThiHT = countGiaHangHoaTaiSieuThiHT.Count();
+
+            // Kê khai giá, 
+
+
+            //Giá sản phẩm dịch vụ cụ thể
+
+            var countGiaSpDvCuTheCHTthang = _db.GiaSpDvCuThe.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaSpDvCuTheCHTthang = countGiaSpDvCuTheCHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaSpDvCuTheCHTthang = countGiaSpDvCuTheCHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaSpDvCuTheCHTthang = countGiaSpDvCuTheCHTthang.Count();
+
+            var countGiaSpDvCuTheHTthang = _db.GiaSpDvCuThe.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaSpDvCuTheHTthang = countGiaSpDvCuTheHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaSpDvCuTheHTthang = countGiaSpDvCuTheHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaSpDvCuTheHTthang = countGiaSpDvCuTheHTthang.Count();
+
+            var countGiaSpDvCuTheCHT = _db.GiaSpDvCuThe.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaSpDvCuTheCHT = countGiaSpDvCuTheCHT.Count();
+
+            var countGiaSpDvCuTheHT = _db.GiaSpDvCuThe.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaSpDvCuTheHT = countGiaSpDvCuTheHT.Count();
+
+
+
+            //Giá sản phẩm dịch vụ tối đa
+
+            var countGiaSpDvToiDaCHTthang = _db.GiaSpDvToiDa.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaSpDvToiDaCHTthang = countGiaSpDvToiDaCHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaSpDvToiDaCHTthang = countGiaSpDvToiDaCHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaSpDvToiDaCHTthang = countGiaSpDvToiDaCHTthang.Count();
+
+            var countGiaSpDvToiDaHTthang = _db.GiaSpDvToiDa.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaSpDvToiDaHTthang = countGiaSpDvToiDaHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaSpDvToiDaHTthang = countGiaSpDvToiDaHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaSpDvToiDaHTthang = countGiaSpDvToiDaHTthang.Count();
+
+            var countGiaSpDvToiDaCHT = _db.GiaSpDvToiDa.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaSpDvToiDaCHT = countGiaSpDvToiDaCHT.Count();
+
+            var countGiaSpDvToiDaHT = _db.GiaSpDvToiDa.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaSpDvToiDaHT = countGiaSpDvToiDaHT.Count();
+
+
+            //Giá sản phẩm dịch vụ khung giá
+
+            var countGiaSpDvKhungGiaCHTthang = _db.GiaSpDvKhungGia.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaSpDvKhungGiaCHTthang = countGiaSpDvKhungGiaCHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaSpDvKhungGiaCHTthang = countGiaSpDvKhungGiaCHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaSpDvKhungGiaCHTthang = countGiaSpDvKhungGiaCHTthang.Count();
+
+            var countGiaSpDvKhungGiaHTthang = _db.GiaSpDvKhungGia.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaSpDvKhungGiaHTthang = countGiaSpDvKhungGiaHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaSpDvKhungGiaHTthang = countGiaSpDvKhungGiaHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaSpDvKhungGiaHTthang = countGiaSpDvKhungGiaHTthang.Count();
+
+            var countGiaSpDvKhungGiaCHT = _db.GiaSpDvKhungGia.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaSpDvKhungGiaCHT = countGiaSpDvKhungGiaCHT.Count();
+
+            var countGiaSpDvKhungGiaHT = _db.GiaSpDvKhungGia.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaSpDvKhungGiaHT = countGiaSpDvKhungGiaHT.Count();
+
+            //Giá đất cụ thể
+
+            var countGiaDatCuTheVlCHTthang = _db.GiaDatCuTheVl.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaDatCuTheVlCHTthang = countGiaDatCuTheVlCHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaDatCuTheVlCHTthang = countGiaDatCuTheVlCHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaDatCuTheVlCHTthang = countGiaDatCuTheVlCHTthang.Count();
+
+            var countGiaDatCuTheVlHTthang = _db.GiaDatCuTheVl.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaDatCuTheVlHTthang = countGiaDatCuTheVlHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaDatCuTheVlHTthang = countGiaDatCuTheVlHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaDatCuTheVlHTthang = countGiaDatCuTheVlHTthang.Count();
+
+            var countGiaDatCuTheVlCHT = _db.GiaDatCuTheVl.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaDatCuTheVlCHT = countGiaDatCuTheVlCHT.Count();
+
+            var countGiaDatCuTheVlHT = _db.GiaDatCuTheVl.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaDatCuTheVlHT = countGiaDatCuTheVlHT.Count();
+
+            //Bảng giá đất
+
+            var countGiaDatDiaBanCHTthang = _db.GiaDatDiaBan.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaDatDiaBanCHTthang = countGiaDatDiaBanCHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaDatDiaBanCHTthang = countGiaDatDiaBanCHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaDatDiaBanCHTthang = countGiaDatDiaBanCHTthang.Count();
+
+            var countGiaDatDiaBanHTthang = _db.GiaDatDiaBan.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaDatDiaBanHTthang = countGiaDatDiaBanHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaDatDiaBanHTthang = countGiaDatDiaBanHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaDatDiaBanHTthang = countGiaDatDiaBanHTthang.Count();
+
+            var countGiaDatDiaBanCHT = _db.GiaDatDiaBan.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaDatDiaBanCHT = countGiaDatDiaBanCHT.Count();
+
+            var countGiaDatDiaBanHT = _db.GiaDatDiaBan.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaDatDiaBanHT = countGiaDatDiaBanHT.Count();
+
+
+            //Giá đất giao dịch thực tế trên thị trường
+
+            var countGiaGiaoDichDatCHTthang = _db.GiaGiaoDichDat.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaGiaoDichDatCHTthang = countGiaGiaoDichDatCHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaGiaoDichDatCHTthang = countGiaGiaoDichDatCHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaGiaoDichDatCHTthang = countGiaGiaoDichDatCHTthang.Count();
+
+            var countGiaGiaoDichDatHTthang = _db.GiaGiaoDichDat.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaGiaoDichDatHTthang = countGiaGiaoDichDatHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaGiaoDichDatHTthang = countGiaGiaoDichDatHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaGiaoDichDatHTthang = countGiaGiaoDichDatHTthang.Count();
+
+            var countGiaGiaoDichDatCHT = _db.GiaGiaoDichDat.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaGiaoDichDatCHT = countGiaGiaoDichDatCHT.Count();
+
+            var countGiaGiaoDichDatHT = _db.GiaGiaoDichDat.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaGiaoDichDatHT = countGiaGiaoDichDatHT.Count();
+
+
+            //Giá trúng đấu giá quyền sử dụng đất
+            var countGiaDauGiaDatCHTthang = _db.GiaDauGiaDat.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaDauGiaDatCHTthang = countGiaDauGiaDatCHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaDauGiaDatCHTthang = countGiaDauGiaDatCHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaDauGiaDatCHTthang = countGiaDauGiaDatCHTthang.Count();
+
+            var countGiaDauGiaDatHTthang = _db.GiaDauGiaDat.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaDauGiaDatHTthang = countGiaDauGiaDatHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaDauGiaDatHTthang = countGiaDauGiaDatHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int tongGiaDauGiaDatHTthang = countGiaDauGiaDatHTthang.Count();
+
+            var countGiaDauGiaDatCHT = _db.GiaDauGiaDat.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaDauGiaDatCHT = countGiaDauGiaDatCHT.Count();
+
+            var countGiaDauGiaDatHT = _db.GiaDauGiaDat.Where(x => (x.Trangthai != "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int tongGiaDauGiaDatHT = countGiaDauGiaDatHT.Count();
 
             var viewModel = new VMThongKe
             {
@@ -269,6 +457,64 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
                 countGiaDvKcbHTthang = tongGiaDvKcbHTthang,
                 countGiaDvKcbCHT = tongGiaDvKcbCHT,
                 countGiaDvKcbHT = tongGiaDvKcbHT,
+
+                countGiaTroGiaTroCuocCHTthang = tongGiaTroGiaTroCuocCHTthang,
+                countGiaTroGiaTroCuocHTthang = tongGiaTroGiaTroCuocHTthang,
+                countGiaTroGiaTroCuocCHT = tongGiaTroGiaTroCuocCHT,
+                countGiaTroGiaTroCuocHT = tongGiaTroGiaTroCuocHT,
+
+
+                countGiaThueTaiNguyenCHTthang = tongGiaThueTaiNguyenCHTthang,
+                countGiaThueTaiNguyenHTthang = tongGiaThueTaiNguyenHTthang,
+                countGiaThueTaiNguyenCHT = tongGiaThueTaiNguyenCHT,
+                countGiaThueTaiNguyenHT = tongGiaThueTaiNguyenHT,
+
+
+                countGiaHangHoaTaiSieuThiCHTthang = tongGiaHangHoaTaiSieuThiCHTthang,
+                countGiaHangHoaTaiSieuThiHTthang = tongGiaHangHoaTaiSieuThiHTthang,
+                countGiaHangHoaTaiSieuThiCHT = tongGiaHangHoaTaiSieuThiCHT,
+                countGiaHangHoaTaiSieuThiHT = tongGiaHangHoaTaiSieuThiHT,
+
+
+                countGiaSpDvCuTheCHTthang = tongGiaSpDvCuTheCHTthang,
+                countGiaSpDvCuTheHTthang = tongGiaSpDvCuTheHTthang,
+                countGiaSpDvCuTheCHT = tongGiaSpDvCuTheCHT,
+                countGiaSpDvCuTheHT = tongGiaSpDvCuTheHT,
+
+
+                countGiaSpDvToiDaCHTthang = tongGiaSpDvToiDaCHTthang,
+                countGiaSpDvToiDaHTthang = tongGiaSpDvToiDaHTthang,
+                countGiaSpDvToiDaCHT = tongGiaSpDvToiDaCHT,
+                countGiaSpDvToiDaHT = tongGiaSpDvToiDaHT,
+
+                countGiaSpDvKhungGiaCHTthang = tongGiaSpDvKhungGiaCHTthang,
+                countGiaSpDvKhungGiaHTthang = tongGiaSpDvKhungGiaHTthang,
+                countGiaSpDvKhungGiaCHT = tongGiaSpDvKhungGiaCHT,
+                countGiaSpDvKhungGiaHT = tongGiaSpDvKhungGiaHT,
+
+                countGiaDatCuTheVlCHTthang = tongGiaDatCuTheVlCHTthang,
+                countGiaDatCuTheVlHTthang = tongGiaDatCuTheVlHTthang,
+                countGiaDatCuTheVlCHT = tongGiaDatCuTheVlCHT,
+                countGiaDatCuTheVlHT = tongGiaDatCuTheVlHT,
+
+                countGiaDatDiaBanCHTthang = tongGiaDatDiaBanCHTthang,
+                countGiaDatDiaBanHTthang = tongGiaDatDiaBanHTthang,
+                countGiaDatDiaBanCHT = tongGiaDatDiaBanCHT,
+                countGiaDatDiaBanHT = tongGiaDatDiaBanHT,
+
+                countGiaGiaoDichDatCHTthang = tongGiaGiaoDichDatCHTthang,
+                countGiaGiaoDichDatHTthang = tongGiaGiaoDichDatHTthang,
+                countGiaGiaoDichDatCHT = tongGiaGiaoDichDatCHT,
+                countGiaGiaoDichDatHT = tongGiaGiaoDichDatHT,
+
+
+                countGiaDauGiaDatCHTthang = tongGiaDauGiaDatCHTthang,
+                countGiaDauGiaDatHTthang = tongGiaDauGiaDatHTthang,
+                countGiaDauGiaDatCHT = tongGiaDauGiaDatCHT,
+                countGiaDauGiaDatHT = tongGiaDauGiaDatHT,
+
+
+
             };
 
             ViewData["Title"] = " Thống kê hồ sơ của đơn vị hành chính";
@@ -276,8 +522,11 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
             ViewData["MenuLv2"] = "menu_thongke_donvi";
             ViewData["DsDonvi"] = _db.DsDonVi;
             ViewData["Nam"] = Nam;
-            ViewData["Madv"] = Madv;
-            ViewData["Thang"] = Thang;
+            //ViewData["Madv"] = Madv;
+            //ViewData["Thang"] = Thang;
+
+            ViewData["Nam"] = Nam == 0 ? DateTime.Now.Year : Nam;
+            ViewData["Thang"] = Thang == 0 ? DateTime.Now.Month : Thang;
 
             return View("Views/Admin/Systems/ThongKe/Index.cshtml", viewModel);
         }
--- CSDLGia_ASP/ViewModels/Systems/VMThongKe.cs
+++ CSDLGia_ASP/ViewModels/Systems/VMThongKe.cs
@@ -63,11 +63,77 @@
         public int countGiaDvGdDtCHT { get; set; }
         public int countGiaDvGdDtHT { get; set; }
 
-        //Giá dịch vụ
+        //Giá dịch vụ khám chữa bệnh
         public int countGiaDvKcbCHTthang { get; set; }
         public int countGiaDvKcbHTthang { get; set; }
         public int countGiaDvKcbCHT { get; set; }
         public int countGiaDvKcbHT { get; set; }
+
+        //Giá dịch vụ trợ giá trợ cước
+        public int countGiaTroGiaTroCuocCHTthang { get; set; }
+        public int countGiaTroGiaTroCuocHTthang { get; set; }
+        public int countGiaTroGiaTroCuocCHT { get; set; }
+        public int countGiaTroGiaTroCuocHT { get; set; }
+
+
+        //Giá thuế tài nguyên
+        public int countGiaThueTaiNguyenCHTthang { get; set; }
+        public int countGiaThueTaiNguyenHTthang { get; set; }
+        public int countGiaThueTaiNguyenCHT { get; set; }
+        public int countGiaThueTaiNguyenHT { get; set; }
+
+        // Giá siêu thị
+        public int countGiaHangHoaTaiSieuThiCHTthang { get; set; }
+        public int countGiaHangHoaTaiSieuThiHTthang { get; set; }
+        public int countGiaHangHoaTaiSieuThiCHT { get; set; }
+        public int countGiaHangHoaTaiSieuThiHT { get; set; }
+
+        // Kê khai giá
+
+        // Giá sản phẩm dịch vụ cụ thể
+        public int countGiaSpDvCuTheCHTthang { get; set; }
+        public int countGiaSpDvCuTheHTthang { get; set; }
+        public int countGiaSpDvCuTheCHT { get; set; }
+        public int countGiaSpDvCuTheHT { get; set; }
+
+        // Giá sản phẩm dịch vụ tối đa
+        public int countGiaSpDvToiDaCHTthang { get; set; }
+        public int countGiaSpDvToiDaHTthang { get; set; }
+        public int countGiaSpDvToiDaCHT { get; set; }
+        public int countGiaSpDvToiDaHT { get; set; }
+
+
+        // Giá sản phẩm dịch vụ khung giá
+        public int countGiaSpDvKhungGiaCHTthang { get; set; }
+        public int countGiaSpDvKhungGiaHTthang { get; set; }
+        public int countGiaSpDvKhungGiaCHT { get; set; }
+        public int countGiaSpDvKhungGiaHT { get; set; }
+
+        // Giá giá đất cụ thể
+        public int countGiaDatCuTheVlCHTthang { get; set; }
+        public int countGiaDatCuTheVlHTthang { get; set; }
+        public int countGiaDatCuTheVlCHT { get; set; }
+        public int countGiaDatCuTheVlHT { get; set; }
+
+        // Giá đất địa bàn
+        public int countGiaDatDiaBanCHTthang { get; set; }
+        public int countGiaDatDiaBanHTthang { get; set; }
+        public int countGiaDatDiaBanCHT { get; set; }
+        public int countGiaDatDiaBanHT { get; set; }
+
+
+        public int countGiaGiaoDichDatCHTthang { get; set; }
+        public int countGiaGiaoDichDatHTthang { get; set; }
+        public int countGiaGiaoDichDatCHT { get; set; }
+        public int countGiaGiaoDichDatHT { get; set; }
+
+        // Giá trúng đấu giá quyền sử dụng đất
+        public int countGiaDauGiaDatCHTthang { get; set; }
+        public int countGiaDauGiaDatHTthang { get; set; }
+        public int countGiaDauGiaDatCHT { get; set; }
+        public int countGiaDauGiaDatHT { get; set; }
+
+
 
 
 
