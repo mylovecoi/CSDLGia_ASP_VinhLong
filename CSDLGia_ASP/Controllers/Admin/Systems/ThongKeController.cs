ec106b3047965b7e5507600fbcebcfbec58d450b
son <quangson.laptrinh@gmail.com>
2024-11-06 11:05:09 +0700

    update

--- CSDLGia_ASP/Controllers/Admin/Systems/ThongKeController.cs
+++ CSDLGia_ASP/Controllers/Admin/Systems/ThongKeController.cs
@@ -28,40 +28,40 @@ namespace CSDLGia_ASP.Controllers.Admin.Systems
         [Route("ThongKe/DanhSach")]
         [HttpGet]
         public IActionResult Index(int Nam, string Madv, int Thang)
-        {
+        {      
 
+            var countGiaThueDatNuocCHTthang = _db.GiaThueMatDatMatNuoc.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaThueDatNuocCHTthang = countGiaThueDatNuocCHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaThueDatNuocCHTthang = countGiaThueDatNuocCHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int countgiathuedatnuocCHTthang = countGiaThueDatNuocCHTthang.Count();
 
+            var countGiaThueDatNuocHTthang = _db.GiaThueMatDatMatNuoc.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            if (Nam > 0) { countGiaThueDatNuocHTthang = countGiaThueDatNuocHTthang.Where(x => x.Thoidiem.Year == Nam); }
+            if (Thang > 0) { countGiaThueDatNuocHTthang = countGiaThueDatNuocHTthang.Where(x => x.Thoidiem.Month == Thang); }
+            int countgiathuedatnuocHTthang = countGiaThueDatNuocHTthang.Count();
 
-            var countGiaThueDatNuocHT = _db.GiaThueMatDatMatNuoc.Where(x =>(x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            var countGiaThueDatNuocCHT = _db.GiaThueMatDatMatNuoc.Where(x => (x.Trangthai == "CC") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv));
+            int countgiathuedatnuocCHT = countGiaThueDatNuocCHT.Count();
 
-            if (Nam > 0) // Nếu Nam lớn hơn 0 thì lọc theo năm
-            {
-                countGiaThueDatNuocHT = countGiaThueDatNuocHT.Where(x => x.Thoidiem.Year == Nam);
-            }
-
-            if (Thang > 0) // Nếu Thang lớn hơn 0 thì lọc theo tháng
-            {
-                countGiaThueDatNuocHT = countGiaThueDatNuocHT.Where(x => x.Thoidiem.Month == Thang);
-            }
-
-            int count = countGiaThueDatNuocHT.Count();
-
-            var countGiaThueDatNuocCHT = _db.GiaThueMatDatMatNuoc.Where(x => x.Trangthai == "CHT" && x.Madv == Madv && x.Thoidiem.Year == Nam).Count();
-
-
-
+            var countGiaThueDatNuocHT = _db.GiaThueMatDatMatNuoc.Where(x => (x.Trangthai == "HT" || x.Trangthai == "CB") && (string.IsNullOrEmpty(Madv) || Madv == "all" || x.Madv == Madv)); 
+            int countgiathuedatnuocHT = countGiaThueDatNuocHT.Count();
 
             var viewModel = new VMThongKe
-            {
-                countGiaThueDatNuocHT = count,
-                countGiaThueDatNuocCHT = countGiaThueDatNuocCHT,
-
+            {              
+                countGiaThueDatNuocCHTthang = countgiathuedatnuocCHTthang,
+                countGiaThueDatNuocHTthang = countgiathuedatnuocHTthang,
+                countGiaThueDatNuocCHT = countgiathuedatnuocCHT,
+                countGiaThueDatNuocHT = countgiathuedatnuocHT,
             };
 
             ViewData["Title"] = " Thống kê hồ sơ của đơn vị hành chính";
             ViewData["MenuLv1"] = "menu_thongke";
             ViewData["MenuLv2"] = "menu_thongke_donvi";
             ViewData["DsDonvi"] = _db.DsDonVi;
+            ViewData["Nam"] = Nam;
+            ViewData["Madv"] = Madv;
+            ViewData["Thang"] = Thang;
+
             return View("Views/Admin/Systems/ThongKe/Index.cshtml", viewModel);
         }
 
